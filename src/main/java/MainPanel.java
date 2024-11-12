97f6cb455a83fc67c0f4c79b396ad1c21c74a5c5
EunJung Kim <dlqnfdl@gmail.com>
2024-11-13 03:33:27 +0900

    update: 배경이미지 개선

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -7,15 +7,16 @@ import javax.swing.*;
 
 public class MainPanel extends JPanel {
 
-    private BufferedImage[] background_images = new BufferedImage[6];
+    private BufferedImage[] background_images = new BufferedImage[10];
     private BufferedImage[] background_middle = new BufferedImage[30];
-    private BufferedImage fooler_block1, fooler_block2, fooler_block3;
+    private BufferedImage fooler_block1, fooler_block2, fooler_block3, logo;
     private JLabel octo1, octo2;
 
+    private int[] imageX = new int[10];
     private int Octo1_imageX = 0;
-    private int Octo2_imageX = 0;
-    private int Octo1_speed = 3;
-    private int Octo2_speed = 2;
+    private int Octo2_imageX = 100;
+    private int Octo1_speed = 4;
+    private int Octo2_speed = 3;
 
     public MainPanel(MainFrame frame) {
         setLayout(null);
@@ -36,25 +37,50 @@ public class MainPanel extends JPanel {
         add(joinRoomButton);
 
         // 캐릭터 이미지 로딩
-        ImageIcon octo1_icon = new ImageIcon("src/main/java/image/대기2.gif");
+        ImageIcon octo1_icon = new ImageIcon("src/main/java/image/대기1.gif");
         ImageIcon octo2_icon = new ImageIcon("src/main/java/image/대기2.gif");
-        Image octo1_scaled = octo1_icon.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
-        Image octo2_scaled = octo2_icon.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
 
-        octo1 = new JLabel(new ImageIcon(octo1_scaled));
-        octo2 = new JLabel(new ImageIcon(octo2_scaled));
-        octo1.setBounds(Octo1_imageX, 570, octo1_icon.getIconWidth(), octo1_icon.getIconHeight());
-        octo2.setBounds(Octo2_imageX, 570, octo2_icon.getIconWidth(), octo2_icon.getIconHeight());
+        octo1 = new JLabel(octo1_icon);
+        octo2 = new JLabel(octo2_icon);
+        octo1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // 상단에 10픽셀 여백 추가
+        octo2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        octo1.setBounds(Octo1_imageX, 550, octo1_icon.getIconWidth(), octo1_icon.getIconHeight());
+        octo2.setBounds(Octo2_imageX, 550, octo2_icon.getIconWidth(), octo2_icon.getIconHeight());
 
         add(octo1);
         add(octo2);
 
+        // 깃발 이미지
+        ImageIcon flag_gif = new ImageIcon("src/main/java/image/깃발.gif");
+        JLabel flag = new JLabel(flag_gif);
+        flag.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        flag.setBounds(1100, 500, flag_gif.getIconWidth(), flag_gif.getIconHeight());
+        add(flag);
+        
+        // 배경 새 이미지
+        ImageIcon bird1_icon = new ImageIcon("src/main/java/image/새1.gif");
+        ImageIcon bird2_icon = new ImageIcon("src/main/java/image/새2.gif");
+        ImageIcon bird3_icon = new ImageIcon("src/main/java/image/새3.gif");
+
+        JLabel bird1 = new JLabel(bird1_icon);
+        JLabel bird2 = new JLabel(bird2_icon);
+        JLabel bird3 = new JLabel(bird3_icon);
+
+        bird1.setBounds(1100, 350, bird1_icon.getIconWidth(), bird1_icon.getIconHeight());
+        bird2.setBounds(1000, 300, bird2_icon.getIconWidth(), bird2_icon.getIconHeight());
+        bird3.setBounds(1150, 250, bird3_icon.getIconWidth(), bird3_icon.getIconHeight());
+        add(bird1);
+        add(bird2);
+        add(bird3);
+
         loadImages();  // 이미지를 한 번만 로드
+
+        System.out.println("Octo1 Image size: " + octo1_icon.getIconWidth() + "x" + octo1_icon.getIconHeight());
 
         revalidate();
         repaint();
 
-        Timer timer = new Timer(5, e -> moveImage());
+        Timer timer = new Timer(15, e -> moveOcto());
         timer.start();
     }
 
@@ -67,6 +93,7 @@ public class MainPanel extends JPanel {
             background_images[3] = ImageIO.read(new File("src/main/java/image/배경_중간2.png"));
             background_images[4] = ImageIO.read(new File("src/main/java/image/배경_중간3.png"));
             background_images[5] = ImageIO.read(new File("src/main/java/image/배경_중간4.png"));
+            background_images[6] = ImageIO.read(new File("src/main/java/image/깃대.png"));
 
             for (int i = 0; i < 22; i++) {
                 int idx = i%4 + 2;
@@ -78,13 +105,18 @@ public class MainPanel extends JPanel {
             fooler_block2 = ImageIO.read(new File("src/main/java/image/풀_바닥2.png"));
             fooler_block3 = ImageIO.read(new File("src/main/java/image/풀_바닥3.png"));
 
+            logo = ImageIO.read(new File("src/main/java/image/logo.png"));
+
+
+            System.out.println(logo);
+
         } catch (IOException e) {
             System.out.println("이미지 로드 오류: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private void moveImage() {
+    private void moveOcto() {
         Octo1_imageX += Octo1_speed;
         Octo2_imageX += Octo2_speed;
         if (Octo1_imageX > getWidth())
@@ -92,10 +124,14 @@ public class MainPanel extends JPanel {
         if (Octo2_imageX > getWidth())
             Octo2_imageX = -octo2.getWidth();
 
-        octo1.setLocation(Octo1_imageX, 570);
-        octo2.setLocation(Octo2_imageX, 570);
+        octo1.setLocation(Octo1_imageX, 550);
+        octo2.setLocation(Octo2_imageX, 550);
         revalidate();
         repaint();  // 화면을 다시 그립니다.
+    }
+
+    private void moveBird() {
+
     }
 
     @Override
@@ -127,5 +163,10 @@ public class MainPanel extends JPanel {
             g.drawImage(fooler_block2, x, 625, 60, 60, this);
         }
         g.drawImage(fooler_block3, width - 60, 625, 60, 60, this);
+
+        g.drawImage(logo, 400, 40, 460, 240, this);
+
+        g.drawImage(background_images[6], 1100, 580, 50, 50, this);
+        g.drawImage(background_images[6], 1100, 540, 50, 50, this);
     }
 }
